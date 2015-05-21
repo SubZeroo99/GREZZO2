ae846b70081bc1c25442399cd6f88dbf35898618
Alex <alexxanx@gmail.com>
2015-05-22 00:00:20 +0200

    settings

---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Annelies
+ * Date: 10-4-2015
+ * Time: 16:49
+ */
+ 
+define("SPOTIFY_CLIENT_ID", "897d796df4614993bd6d57819e85c941");
+define("SPOTIFY_CLIENT_SECRET", "aa67320920d5492089f4c9e48f4935ec");
+define ("REDIRECT_URL" , "http://localhost/imp03");
