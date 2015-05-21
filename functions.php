4d5224cc81643afa5621b32297e6d39e0ae16e58
Alex <alexxanx@gmail.com>
2015-05-21 23:55:37 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,48 +1,26 @@
-add_action( 'wp_enqueue_scripts', 'cyb_enqueue_scripts' );
-function cyb_enqueue_scripts() {
-    //Change the key and url with yours
-    wp_register_script('my-js', get_stylesheet_directory_uri(). '/js/my-js.js', array( 'jquery' ) );
-    wp_enqueue_script('my-js');
- 
-    //Localize script data to be used in my-js.js
-    $scriptData = array();
-    $scriptData['ajaxurl'] = admin_url( 'admin-ajax.php' );
-    $scriptData['action'] = 'save_goals_category';
- 
-    wp_localize_script( 'my-js', 'my_js_data', $scriptData );
- 
-}
- 
-add_action("wp_ajax_save_goals_category", "save_goals_category");
-add_action("wp_ajax_save_goals_category", "save_goals_category");
- 
-function save_goals_category(){
-       
-        $user_id = get_current_user_id();
-        if ($user_id == 0) {
-                echo 'You are currently not logged in.';
-        } else {
-                $catarr = array(
-                                                  'cat_name' => $_POST['addCatText'],
-                                                  'category_description' => 'Users Category',
-                                                  'category_nicename' => '',
-                                                  'category_parent' => '',
-                                                  'taxonomy' => 'goals-category'
-                                                  );
-         
-                $my_cat_id = wp_insert_category( $catarr, $wp_error );
-               
-                if($my_cat_id !=0){
-                        $meta_key = 'cats';
-                         $new_meta_key = get_user_meta($user_id, $meta_key,true);
-                        if( ! is_array( $new_meta_key ) ) $new_meta_key = array();
-                        $new_meta_key[] = $my_cat_id;
-                        $meta_value = $new_meta_key;
-                        $updateMetaVal = update_user_meta( $user_id, $meta_key, $meta_value);
-                        echo json_encode(array('result'=>'success','msg'=>'Category saved successfully','newCategoryId'=>$my_cat_id,'newCategoryName'=>$_POST['addCatText']));
-                }else{
-                        echo json_encode(array('result'=>'error','msg'=>'Error in saving category'));
-                }
-        }
-        exit;
-}
+//      Average Days from “Repairs in Progress” to “Repairs Completed” Credits to LegitButton
+        $qb = $em->getRepository('AccMainBundle:Job')
+                ->createQueryBuilder('j')
+                ->select('AVG(ABS(TIMESTAMPDIFF(DAY, jsi1.createdAt, jsi2.createdAt)))')
+                ->join('j.statuses', 'jsi1')
+                ->join('jsi1.status', 'js1')
+                ->join('js1.category', 'jsc1', 'WITH', 'jsc1.code = :firstStatusCategoryCode')
+                ->join('j.statuses', 'jsi2')
+                ->join('jsi2.status', 'js2')
+                ->join('js2.category', 'jsc2', 'WITH', 'jsc2.code = :secondStatusCategoryCode')
+                ->andWhere('(SELECT COUNT(jsi1_t.id) FROM AccMainBundle:JobStatusItem jsi1_t JOIN AccMainBundle:JobStatus js1_t WITH js1_t = jsi1_t.status JOIN AccMainBundle:JobStatusCategory jsc1_t WITH jsc1_t = js1_t.category WHERE jsi1_t.job = j AND jsc1_t.code = jsc1.code AND jsi1_t.createdAt < jsi1.createdAt) = 0')
+                ->andWhere('(SELECT COUNT(jsi2_t.id) FROM AccMainBundle:JobStatusItem jsi2_t JOIN AccMainBundle:JobStatus js2_t WITH js2_t = jsi2_t.status JOIN AccMainBundle:JobStatusCategory jsc2_t WITH jsc2_t = js2_t.category WHERE jsi2_t.job = j AND jsc2_t.code = jsc2.code AND jsi2_t.createdAt > jsi2.createdAt) = 0')
+                ->andWhere('j.createdAt BETWEEN :from AND :to')
+                ->setParameters($params)
+                ->setParameter('firstStatusCategoryCode', JobStatusCategory::REPAIRS_IN_PROGRESS)
+                ->setParameter('secondStatusCategoryCode', JobStatusCategory::REPAIRS_COMPLETED)
+                ;
+                $this->filterJobStatus($qb);
+                $this->filter($qb, $bodyshop, $insuranceCompany);
+                $report['Cycle Time']['Average Days from “Repairs in Progress” to “Repairs Completed”']['Combined'] =
+                        $qb->getQuery()->getSingleScalarResult();
+                $qb->andWhere('j.carDrivable = :carDrivable');
+                $report['Cycle Time']['Average Days from “Repairs in Progress” to “Repairs Completed”']['Drivable'] =
+                        $qb->setParameter('carDrivable', true)->getQuery()->getSingleScalarResult();
+                $report['Cycle Time']['Average Days from “Repairs in Progress” to “Repairs Completed”']['NonDrivable'] =
+                        $qb->setParameter('carDrivable', false)->getQuery()->getSingleScalarResult();
